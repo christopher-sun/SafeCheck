fa74c40b9b857b5f6ec44ab2166feca2e3b03f29
David Zhao <david_zhao98@msn.com>
2017-10-14 08:49:48 -0400

    added account class and firebase connectivity

--- app/src/main/java/cddj/gatech/edu/safecheck/MainActivity.java
+++ app/src/main/java/cddj/gatech/edu/safecheck/MainActivity.java
@@ -8,10 +8,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button registerButton;
-    //private EditText userName;
+    private EditText userName;
     private EditText userPhone;
 
     private static String username;
@@ -23,16 +26,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         registerButton = (Button) findViewById(R.id.registerButton);
-        //userName = (EditText) findViewById(R.id.userName);
+        userName = (EditText) findViewById(R.id.userName);
         userPhone = (EditText) findViewById(R.id.userPhone);
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!getUserPhone().isEmpty()) {
+                if (!getUserPhone().isEmpty() && !getUsername().isEmpty()) {
                     userPhoneNum = getUserPhone();
+                    username = getUsername();
+
+                    final Account newAccount = new Account(userPhoneNum, username);
+                    newAccount.getContacts().add("+14254453074");
+                    newAccount.getContacts().add("+18882345678");
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference myRef = database.getReference("users");
+
+                    myRef.child(newAccount.getNumber()).setValue(newAccount);
+
                     Intent intent = new Intent(view.getContext(), HomeActivity.class);
                     startActivity(intent);
+
                 } else {
                     Toast.makeText(view.getContext(), "Invalid Phone Number", Toast.LENGTH_SHORT).show();
                 }
@@ -42,9 +57,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    public String getUsername() {
-//        return userName.getText().toString();
-//    }
+    public String getUsername() {
+        return userName.getText().toString();
+    }
 
     public String getUserPhone() {
         return userPhone.getText().toString();
