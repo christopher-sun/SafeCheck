a7de5f62ee2eff88d21a07a4eac53acbdaf82df4
Julia Chen <jchen694@gatech.edu>
2017-10-14 13:19:26 -0400

    preliminary working, need to add contacts

--- app/src/main/java/cddj/gatech/edu/safecheck/SendTextActivity.java
+++ app/src/main/java/cddj/gatech/edu/safecheck/SendTextActivity.java
@@ -7,22 +7,28 @@ import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by juliachen on 10/14/17.
  */
 
-class SendTextActivity extends AppCompatActivity {
+public class SendTextActivity extends AppCompatActivity {
 
     private EditText editText;
+    private static final String twilioNum = "+12064830490";
     private Button sendButton;
 
-    private SmsManager smsManager = SmsManager.getDefault();
+    private SmsManager smsManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_text);
+
+
+
+        smsManager = SmsManager.getDefault();
 
         editText = (EditText) findViewById(R.id.editText);
         sendButton = (Button) findViewById(R.id.sendButton);
@@ -30,6 +36,21 @@ class SendTextActivity extends AppCompatActivity {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String phoneNo = twilioNum;
+                String sms = getText();
+
+                try {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+                    Toast.makeText(getApplicationContext(), "SMS Sent!",
+                            Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS faild, please try again later!",
+                            Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+                //sendMessage(editText.getText().toString());
             }
         });
 
@@ -39,4 +60,39 @@ class SendTextActivity extends AppCompatActivity {
     public String getText() {
         return editText.getText().toString();
     }
+
+
+//    private void sendMessage(String text) {
+//        if (ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.SEND_SMS)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+//                    Manifest.permission.SEND_SMS)) {
+//            } else {
+//                ActivityCompat.requestPermissions(this,
+//                        new String[]{Manifest.permission.SEND_SMS},
+//                        MY_PERMISSIONS_REQUEST_SEND_SMS);
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+//        switch (requestCode) {
+//            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+//                if (grantResults.length > 0
+//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    SmsManager smsManager = SmsManager.getDefault();
+//                    smsManager.sendTextMessage(twilioNum, null, getText(), null, null);
+//                    Toast.makeText(getApplicationContext(), "SMS sent.",
+//                            Toast.LENGTH_LONG).show();
+//                } else {
+//                    Toast.makeText(getApplicationContext(),
+//                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+//                    return;
+//                }
+//            }
+//        }
+//
+//    }
 }
