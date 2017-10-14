33c5996fb98e4d5ba79be701c624f064e8208d39
Julia Chen <jchen694@gatech.edu>
2017-10-14 06:25:35 -0400

    inital Twilio

--- javascript/app.js
+++ javascript/app.js
@@ -9,37 +9,37 @@ var numbers = ['+14254453074', '+16788231926'];
 
 for( var i = 0; i < numbers.length; i++ ) {
     client.messages.create({
-        body: 'fuck wid it',
+        body: 'yo',
         to: numbers[i],  // Text this number
         from: '+12064830490' // From a valid Twilio number
     })
     .then((message) => console.log(message.sid));
 }
-//
-//var express = require('express'),
-//bodyParser = require('body-parser'),
-//app = express();
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({
-//    extended: true
-//}));
-//
-////app.post('/message', function (req, res) {
-////    var resp = new twilio.TwimlResponse();
-////    resp.message('Thanks for subscribing!');
-////    res.writeHead(200, {
-////        'Content-Type':'text/xml'
-////    });
-////    res.end(resp.toString());
-////});
-//
-//var server = app.listen(3000, function() {
-//    console.log('Listening on port %d', server.address().port);
+
+var express = require('express'),
+bodyParser = require('body-parser'),
+app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//app.post('/message', function (req, res) {
+//    var resp = new twilio.TwimlResponse();
+//    resp.message('Thanks for subscribing!');
+//    res.writeHead(200, {
+//        'Content-Type':'text/xml'
+//    });
+//    res.end(resp.toString());
 //});
-//
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
+
 //var Firebase = require('firebase'),
 //usersRef = new Firebase('https://safecheck-2017.firebaseio.com/Users/');
-//
+
 //var config = {
 //    apiKey: "AIzaSyCfzPkYsLtY1adV3U5ZLqrCm0soAFYJUFk",
 //    authDomain: "safecheck-2017.firebaseapp.com",
@@ -49,29 +49,29 @@ for( var i = 0; i < numbers.length; i++ ) {
 //    messagingSenderId: "752359854545"
 //};
 //firebase.initializeApp(config);
-//
+
 //var numbers = [];
 //usersRef.on('child_added', function(snapshot) {
 //numbers.push( snapshot.val() );
 //  console.log( 'Added number ' + snapshot.val() );
 //});
-//
-//app.post('/message', function (req, res) {
-//    var resp = new twilio.TwimlResponse();
-//    if( req.body.Body.trim().toLowerCase() === 'subscribe' ) {
-//        var fromNum = req.body.From;
-//        if(numbers.indexOf(fromNum) !== -1) {
-//            resp.message('You already subscribed!');
-//        } else {
-//            resp.message('Thank you, you are now subscribed. Reply "STOP" to stop receiving updates.');
-//            usersRef.push(fromNum);
-//        }
-//    } else {
-//        resp.message('Welcome to Daily Updates. Text "Subscribe" receive updates.');
-//    }
-//
-//    res.writeHead(200, {
-//        'Content-Type':'text/xml'
-//    });
-//    res.end(resp.toString());
-//});
+
+app.post('/message', function (req, res) {
+    var resp = new twilio.twiml.MessagingResponse();
+    if( req.body.Body.trim().toLowerCase() === 'subscribe' ) {
+        var fromNum = req.body.From;
+        if(numbers.indexOf(fromNum) !== -1) {
+            resp.message('You already subscribed!');
+        } else {
+            resp.message('Thank you, you are now subscribed. Reply "STOP" to stop receiving updates.');
+            usersRef.push(fromNum);
+        }
+    } else {
+        resp.message('Welcome to Daily Updates. Text "Subscribe" receive updates.');
+    }
+
+    res.writeHead(200, {
+        'Content-Type':'text/xml'
+    });
+    res.end(resp.toString());
+});
